9cc90a005a48359c190be5cf6eb382d8261f4ba6
Richard Minsoo Go <kauntach@naver.com>
2024-04-27 13:28:23 +0900

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from models.vgg import *
+from models.resnet import *
+from models.vit import ViT
+from models.simple_vit import SimpleViT
+
+from models.mae import MAE
+from models.dino import Dino
+
