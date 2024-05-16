7c059002b083399817f674dc0a14b2dee6b58e47
Richard Minsoo Go <kauntach@naver.com>
2024-05-16 12:35:44 +0900

    Update train.py

--- train.py
+++ train.py
@@ -501,7 +501,7 @@ elif args.net=="rvt":
     net = RvT(
         image_size = 224,
         patch_size = 32,
-        num_classes = n_classes,
+        num_classes = 10,
         dim = 1024,
         depth = 6,
         heads = 16,
@@ -535,32 +535,81 @@ elif args.net=="sep_vit":
         dropout = 0.1           # dropout
     )
 
-"""
-
-elif args.net=="xxxx":
-    from models.xxxx import xxxx
-    net = xxxx(
+elif args.net=="twins_svt":
+    from models.twins_svt import TwinsSVT
+    net = TwinsSVT(
+        num_classes = 10,       # number of output classes
+        s1_emb_dim = 64,          # stage 1 - patch embedding projected dimension
+        s1_patch_size = 4,        # stage 1 - patch size for patch embedding
+        s1_local_patch_size = 7,  # stage 1 - patch size for local attention
+        s1_global_k = 7,          # stage 1 - global attention key / value reduction factor, defaults to 7 as specified in paper
+        s1_depth = 1,             # stage 1 - number of transformer blocks (local attn -> ff -> global attn -> ff)
+        s2_emb_dim = 128,         # stage 2 (same as above)
+        s2_patch_size = 2,
+        s2_local_patch_size = 7,
+        s2_global_k = 7,
+        s2_depth = 1,
+        s3_emb_dim = 256,         # stage 3 (same as above)
+        s3_patch_size = 2,
+        s3_local_patch_size = 7,
+        s3_global_k = 7,
+        s3_depth = 5,
+        s4_emb_dim = 512,         # stage 4 (same as above)
+        s4_patch_size = 2,
+        s4_local_patch_size = 7,
+        s4_global_k = 7,
+        s4_depth = 4,
+        peg_kernel_size = 3,      # positional encoding generator kernel size
+        dropout = 0.              # dropout
     )
 
-elif args.net=="xxxx":
-    from models.xxxx import xxxx
-    net = xxxx(
+elif args.net=="vit_with_patch_dropout":
+    from models.vit_with_patch_dropout import ViT
+    net = ViT(
+        image_size = 224,
+        patch_size = 32,
+        num_classes = 10,
+        dim = 1024,
+        depth = 6,
+        heads = 16,
+        mlp_dim = 2048,
+        dropout = 0.1,
+        emb_dropout = 0.1,
+        patch_dropout = 0.25
     )
 
-elif args.net=="xxxx":
-    from models.xxxx import xxxx
-    net = xxxx(
+elif args.net=="vit_with_patch_merger":
+    from models.vit_with_patch_merger import ViT
+    net = ViT(
+        image_size = 256,
+        patch_size = 16,
+        num_classes = 10,
+        dim = 1024,
+        depth = 12,
+        heads = 8,
+        patch_merge_layer = 6,        # at which transformer layer to do patch merging
+        patch_merge_num_tokens = 8,   # the output number of tokens from the patch merge
+        mlp_dim = 2048,
+        dropout = 0.1,
+        emb_dropout = 0.1
     )
 
-elif args.net=="xxxx":
-    from models.xxxx import xxxx
-    net = xxxx(
+elif args.net=="xcit":
+    from models.xcit import XCiT
+    net = XCiT(
+        image_size = 256,
+        patch_size = 32,
+        num_classes = 10,
+        dim = 1024,
+        depth = 12,                     # depth of xcit transformer
+        cls_depth = 2,                  # depth of cross attention of CLS tokens to patch, attention pool at end
+        heads = 16,
+        mlp_dim = 2048,
+        dropout = 0.1,
+        emb_dropout = 0.1,
+        layer_dropout = 0.05,           # randomly dropout 5% of the layers
+        local_patch_kernel_size = 3     # kernel size of the local patch interaction module (depthwise convs)
     )
-
-
-
-"""
-
 
 
 
